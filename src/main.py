c0077cd8b780546f0c083be5ae1b803009d86102
Duy Nguyen <ngdangduy2009@gmail.com>
2024-10-19 00:33:46 +1100

    Refactor main.py: Remove unused import

--- src/main.py
+++ src/main.py
@@ -1,6 +1,5 @@
 
 from controllers.UniversityController import UniversityController
-from models.Validator import Validator
 
 if __name__ == '__main__':
     university_controller = UniversityController()
