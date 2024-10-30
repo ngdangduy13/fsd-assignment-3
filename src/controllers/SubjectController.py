dc8a525e04ff6cc46d561dba04624b5c9ec59acb
Duy Nguyen <ngdangduy2009@gmail.com>
2024-10-30 17:46:51 +1100

    Refactor SubjectController and Student model: Improve subject enrollment logic and add logging

--- src/controllers/SubjectController.py
+++ src/controllers/SubjectController.py
@@ -2,6 +2,7 @@ from utils.Logger import Logger
 from models.Subject import Subject
 from models.Validator import Validator
 from models.Database import Database
+from utils.String import StringUtils
 
 class SubjectEnrollment:
     def __init__(self,student):
@@ -10,8 +11,10 @@ class SubjectEnrollment:
     def enrol_subject(self):
         subject = Subject()
         self.student.enroll_subject(subject)
+        Logger.log_yellow(f"Enrolling in Subject-{subject.id}")
         db = Database()
         db.update_student(self.student)
+        Logger.log_yellow(f"You are enrolled in {len(self.enrolled_subjects)} out of {self.max_subjects} subjects")
 
     def remove_subject(self, subject_id):
         self.student.remove_subject(subject_id)
@@ -19,21 +22,25 @@ class SubjectEnrollment:
         db.update_student(self.student)
     
     def show_enrolled_subjects(self):
-        self.student.show_enrolled_subjects()
+        if not self.student.enrolled_subjects:
+            Logger.log_yellow("Showing 0 subjects")
+        else:
+            Logger.log_yellow(f"Showing {len(self.student.enrolled_subjects)} subjects")
+            for subject in self.student.enrolled_subjects:
+                print(subject)
     
     def change_password(self):
-        print("Updating password")
+        Logger.log_yellow("Updating password")
         new_password = input("New Password: ")
         confirm_password = input("Confirm Password: ")
         self.student.change_password(new_password, confirm_password)
         db = Database()
         db.update_student(self.student)
-        print("Password updated successfully")
 
     def course_menu(self):
         while True:
             try: 
-                user_input = input("Student Course Menu (c/e/r/s/x): ").lower()
+                user_input = input(StringUtils.to_cyan_string("Student Course Menu (c/e/r/s/x): ")).lower()
                 if user_input == 'e':
                     self.enrol_subject()
                 elif user_input == 'r':
--- src/models/Student.py
+++ src/models/Student.py
@@ -25,25 +25,15 @@ class Student:
     def enroll_subject(self, subject):
         if len(self.enrolled_subjects) < self.max_subjects:
             self.enrolled_subjects.append(subject)
-            print(f"Enrolling in Subject-{subject.id}")
-            print(f"You are enrolled in {len(self.enrolled_subjects)} out of {self.max_subjects} subjects")
         else:
-            print("Students are allowed to enrol in 4 subjects only")
-    
-    def show_enrolled_subjects(self):
-        if not self.enrolled_subjects:
-            print("Showing 0 subjects")
-        else:
-            print(f"Showing {len(self.enrolled_subjects)} subjects")
-            for subject in self.enrolled_subjects:
-                print(subject)
+            raise Exception("Students are allowed to enrol in 4 subjects only")
     
     def remove_subject(self, subject_id):
         if subject_id in [subject.id for subject in self.enrolled_subjects]:
             self.enrolled_subjects = [subject for subject in self.enrolled_subjects if subject.id != subject_id]
             print(f"Dropping Subject - {subject_id}")
         else:
-            print(f"Subject - {subject_id} not found!")
+            raise Exception(f"Subject - {subject_id} not found!")
     
     def change_password(self, new_password, confirm_password):
         if new_password != confirm_password:
