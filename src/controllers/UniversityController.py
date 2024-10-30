d1485e458b3b3d74bf841db06266c528cad7b357
ThiHHuynh <thihuynh479@gmail.com>
2024-10-30 21:39:30 +1100

    Update UniversityController.py

--- src/controllers/UniversityController.py
+++ src/controllers/UniversityController.py
@@ -2,19 +2,20 @@ from controllers.AdminController import AdminController
 from controllers.StudentController import StudentController
 from utils.String import StringUtils
 
-
 class UniversityController:
+    # Main method providing the main menu for choosing between Admin, Student, or Exit options.
     def main(self):
-        while (True):
+        while True:
+            # Prompts the user to select an option in the university system menu.
             option = input(StringUtils.to_cyan_string("University System: (A)Admin, (S)Student, or X: "))
-            if option.lower()  == "a":
-                admin_controller = AdminController()
-                admin_controller.main()
-                continue
+            if option.lower() == "a":
+                admin_controller = AdminController()  # Initializes an AdminController instance.
+                admin_controller.main()  # Calls the main method for admin actions.
+                continue  # Returns to the main menu after admin operations.
             elif option.lower() == "s":
-                student_controller = StudentController()
-                student_controller.main()
+                student_controller = StudentController()  # Initializes a StudentController instance.
+                student_controller.main()  # Calls the main method for student actions.
             elif option.lower() == "x":
-                break
+                break  # Exits the loop if "x" is selected.
             else:
-                print("Invalid option")
+                print("Invalid option")  # Notifies the user of an invalid option.
